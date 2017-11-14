b9befb86b192ba3a12847da0de59cb35f2842d4e
unknown <thor.hoegsbro@gmail.com>
2017-11-14 15:08:42 +0100

    Nu er det "Game" i stedet for person.

---
+++ KrydsOgBolle/KrydsOgBolle/GameState.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrydsOgBolle
+{
+    public class GameState : TableEntity
+    {
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public string BoardRow1 { get; set; }
+        public string BoardRow2 { get; set; }
+        public string BoardRow3 { get; set; }
+    }
+}
